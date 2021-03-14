83d6f7017cd003d651aca29162879e5b5d3fe2e6
Ernesto Ormeño <eormeno12@gmail.com>
2021-03-13 20:20:19 -0500

    Navegación principal funcional.
    - Navegación entre fragments mediante el bottom navigation.
    - Bottom navigation con su respectiva UI.
    
    Analizar:
    - Cambio del iconTint del bottom navigation, de colorPrimary a colorPrimaryDark.

--- app/src/main/java/com/listen/to/miskiatty/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/listen/to/miskiatty/view/ui/activities/MainActivity.kt
@@ -2,11 +2,28 @@ package com.listen.to.miskiatty.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.listen.to.miskiatty.R
+import com.listen.to.miskiatty.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configNav()
+    }
+
+    fun configNav(){
+        NavigationUI.setupWithNavController(
+                binding.bnvMain,
+                Navigation.findNavController(
+                        this,
+                        R.id.fr_forAllFragments_main))
     }
 }
