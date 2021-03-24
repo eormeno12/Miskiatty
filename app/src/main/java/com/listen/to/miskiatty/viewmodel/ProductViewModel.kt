e33347b8f24c61397fb3b6ef218096705792af02
Ernesto Ormeño <eormeno12@gmail.com>
2021-03-24 07:52:34 -0500

    Cambio del botón para añadir productos por uno flotante. Antes se encontraba en el menu del action bar.

--- app/src/main/java/com/listen/to/miskiatty/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/listen/to/miskiatty/viewmodel/ProductViewModel.kt
@@ -1,6 +1,8 @@
 package com.listen.to.miskiatty.viewmodel
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
@@ -8,11 +10,13 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
 import com.listen.to.miskiatty.R
 import com.listen.to.miskiatty.model.database.Product
 import com.listen.to.miskiatty.model.adapters.ProductCustomAdapter
 import com.listen.to.miskiatty.model.adapters.ProductsListener
 import com.listen.to.miskiatty.model.repository.products.ProductObservable
+import com.listen.to.miskiatty.view.ui.products.ProductAddActivity
 
 class ProductViewModel: ViewModel() {
 
@@ -21,27 +25,27 @@ class ProductViewModel: ViewModel() {
     private var _productClicked = MutableLiveData<Product>()
     val productClicked: LiveData<Product> = _productClicked
 
-    fun getRecyclerProductsAdapter(): ProductCustomAdapter?{
+    fun getRecyclerProductsAdapter(): ProductCustomAdapter? {
         return productsAdapter
     }
 
-    fun setRecyclerProductsAdapter(){
+    fun setRecyclerProductsAdapter() {
         productsAdapter = ProductCustomAdapter(this, R.layout.template_product,
-                object : ProductsListener{
+                object : ProductsListener {
                     override fun onClickListenerProduct(position: Int) {
                         _productClicked.value = getProductAt(position)
                     }
-        })
+                })
     }
 
-    fun callProducts(appContext: Context, lifecycle: Lifecycle){
+    fun callProducts(appContext: Context, lifecycle: Lifecycle) {
         productObservable.callProductsROOM(appContext, lifecycle)
     }
 
     fun getProducts(): MutableLiveData<List<Product>> = productObservable.getProducts()
 
-    fun setProductsInRecyclerAdapter(products: List<Product>){
-        if(productsAdapter != null){
+    fun setProductsInRecyclerAdapter(products: List<Product>) {
+        if (productsAdapter != null) {
             productsAdapter?.setProductsList(products)
             productsAdapter?.notifyDataSetChanged()
         }
@@ -50,9 +54,13 @@ class ProductViewModel: ViewModel() {
     fun getProductAt(position: Int): Product? =
             productsAdapter?.getProductList()?.get(position)
 
-    fun searchProduct(str: String){
+    fun searchProduct(str: String) {
         productsAdapter?.search(str)
     }
+
+    fun onClickAddProduct(context: Context) =
+            context.startActivity(Intent(context.applicationContext, ProductAddActivity::class.java))
+
 }
 
 @BindingAdapter("srcBitmap")
